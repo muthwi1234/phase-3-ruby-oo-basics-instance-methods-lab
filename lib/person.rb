a773ac49f22f371f8d655e3a2c79fe926afa470f
Peter <peterkilungya1@gmail.com>
2022-10-20 22:19:58 +0300

    assignment completed

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+    def talk
+        puts "Hello World!"
+    end
+    def walk
+        puts "The Person is walking"
+    end
+end
+
+henry=Person.new
+henry.talk
+
+junnie=Person.new
+junnie.walk
